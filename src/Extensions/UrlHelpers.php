6883a73d4f073665885c71beb857429dc859eae7
Magnus Eriksson <mange@reloop.se>
2016-09-08 16:58:58 +0200

    Added asset helper

--- src/Extensions/UrlHelpers.php
+++ src/Extensions/UrlHelpers.php
@@ -1,5 +1,6 @@
 <?php namespace Glue\Plates\Extensions;
 
+use Glue\Config\Config;
 use Glue\Http\Request;
 use Glue\Router\Router;
 use League\Plates\Engine;
@@ -8,6 +9,11 @@ use League\Plates\Extension\URI;
 
 class UrlHelpers implements ExtensionInterface
 {
+    /**
+     * @var Glue\Config\Config
+     */
+    protected $config;
+
     /**
      * @var Glue\Router\Router
      */
@@ -20,11 +26,13 @@ class UrlHelpers implements ExtensionInterface
 
 
     /**
+     * @param Glue\Config\Config $config
      * @param Glue\Router\Router $request
      * @param Glue\Http\Request  $router
      */
-    public function __construct(Request $request, Router $router)
+    public function __construct(Config $config, Request $request, Router $router)
     {
+        $this->config  = $config;
         $this->request = $request;
         $this->router  = $router;
     }
@@ -37,15 +45,16 @@ class UrlHelpers implements ExtensionInterface
     {
         // Load the built in URI extension
         $engine->loadExtension(new URI($this->request->getPathInfo()));
-        
-        // Custom functions
+
+        // Custom methods
         $engine->registerFunction('route', [$this, 'route']);
+        $engine->registerFunction('asset', [$this, 'asset']);
     }
 
 
     /**
      * Resolve a named route
-     * 
+     *
      * @param  string  $name
      * @param  array   $params
      * @param  boolean $absolute    Return an absolute path
@@ -60,4 +69,25 @@ class UrlHelpers implements ExtensionInterface
     }
 
 
+    /**
+     * Get asset path
+     *
+     * @param  string $filename
+     * @return string
+     */
+    public function asset($filename = null)
+    {
+        $path = rtrim($this->config->get('plates.assets'), '/');
+
+        if ($path && strpos($path, '://') === false && strpos($path, '//') !== 0) {
+            $path = '/' . ltrim($path, '/');
+        }
+
+        $filename = ltrim($filename, '/');
+        if (!$filename) {
+            return $path;
+        }
+
+        return $path . '/' . $filename;
+    }
 }
